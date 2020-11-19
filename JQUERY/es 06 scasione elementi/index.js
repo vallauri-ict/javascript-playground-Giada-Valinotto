9b398769a447798e0059461e8f252e6bc0879edf
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2020-11-19 12:06:41 +0100

    Update

--- JQUERY/es 06 scasione elementi/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-let _wrapper;
-//le funz. rihiamate da html vanno fuori
-function evidenzia(selector){
-_wrapper.children(selector).ccs({"backgroundColor":"#FF0"})
-}
-$(document).ready(function() {
-_wrapper=$("#wrapper");
-});
